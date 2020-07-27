32e9cd6067080cad9dfdc6aa19a99a9600ba6f88
HemersonTacon <hemersontacon@gmail.com>
2020-07-27 19:36:51 +0100

    Player jumps

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -15,11 +15,16 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
-            rb.velocity = new Vector2(-5, rb.velocity.y);
+            rb.velocity = new Vector2(-5f, rb.velocity.y);
         }
 
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
-            rb.velocity = new Vector2(5, rb.velocity.y);
+            rb.velocity = new Vector2(5f, rb.velocity.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 5f);
         }
     }
 }
