943a6fd0d2f5dc705335949f2e39ca863ff2fa1f
HemersonTacon <hemersontacon@gmail.com>
2020-07-27 19:29:05 +0100

    Player moves left and right

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+            rb.velocity = new Vector2(-5, rb.velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+            rb.velocity = new Vector2(5, rb.velocity.y);
+        }
+    }
+}
